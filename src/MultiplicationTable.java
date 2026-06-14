fe851c129aa54d50f4894f9959febcd9542468fb
Rajvardhan <rajvardhanr2005@gmail.com>
2026-06-14 09:23:23 +0530

    welcome multiplication example

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("welcome multiplication world");
+        System.out.print("please enter your number");
+        int num = input.nextInt();
+        printmultiplicationtable(num);
+    }
+        public static void printmultiplicationtable(int num){
+            int i=0;
+            while(i <= 10){
+                int sum=num*i;
+                System.out.println(num + "X" + i + "=" + (num*i));
+                i++;
+            }
+        }
+    }
+

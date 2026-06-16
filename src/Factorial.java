87fdf46d8eb6afd692c68a066249ad1900833f3e
Rajvardhan <rajvardhanr2005@gmail.com>
2026-06-16 08:12:45 +0530

    Factorial Example

---
+++ src/Factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Welcome to Factorial");
+        System.out.print("Please enter your number: ");
+        int num = input.nextInt();
+        long fact=factorial(num);
+        System.out.println("Factorial is: " + fact);
+    }
+    public static long factorial(int num){
+        if(num < 2){
+            return 1;
+        }
+        long fact=1;
+        int i=1;
+        while(i<=num) {
+            fact = fact * i;
+            i++;
+
+        }return fact;
+    }
+}

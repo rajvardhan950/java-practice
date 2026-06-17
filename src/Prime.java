3252683e19aafad5e46668a38120b325e7fb9d52
Rajvardhan <rajvardhanr2005@gmail.com>
2026-06-17 08:52:55 +0530

    Welcome to Prime Number checker

---
+++ src/Prime.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Prime Number checker");
+        System.out.print("Please enter your number: ");
+        int num = input.nextInt();
+        boolean isPrime = isPrime(num);
+        if (isPrime) {
+            System.out.println("Your number is Prime");
+        } else {
+            System.out.println("Your number is not prime");
+        }
+    }
+    public static boolean isPrime(int num){
+        int i=2;
+        while (i < num){
+            if(num % i == 0) {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
